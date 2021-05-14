eb22e3a6c4adccb30927a6cc421cbc084859b414
yeaboi19 <lolhaha544@gmail.com>
2021-05-14 11:16:00 +0400

    Upload

--- src/main/java/Token/UnfinishedProjects/TestFun.java
+++ src/main/java/Token/UnfinishedProjects/TestFun.java
@@ -7,6 +7,7 @@ import Token.MainThings.Constants;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.entities.IPermissionHolder;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.role.RoleCreateEvent;
@@ -20,53 +21,79 @@ import java.util.List;
 public class TestFun extends ListenerAdapter {
     Splitter splitter = new Splitter();
     AccessControl ac = new AccessControl();
-    String roleId;
+    RoleStuff rs = new RoleStuff();
+    String msg;
+    String index;
+    String other;
+    MessageChannel channel;
     private IPermissionHolder IPermissionHolder;
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        try {
-            RoleStuff rs = new RoleStuff();
-            String msg = event.getMessage().getContentRaw();
-            String index = splitter.Splitter(msg, 1);
-            String other = splitter.Splitter(msg, 2);
-            if (event.getGuild().getRolesByName("Muted", true).size() == 0) {
-                event.getGuild().createRole().setPermissions(Permission.UNKNOWN).setName("Muted").setColor(new Color(255, 255, 255)).queue();
-            }
+        msg = event.getMessage().getContentRaw();
+        index = splitter.Splitter(msg, 1);
+        other = splitter.Splitter(msg, 2);
+        channel = event.getChannel();
 
-
-            if (ac.AccessControl(event.getAuthor().getId())) {
-                if (index.equalsIgnoreCase(Constants.BotPrefix + "mute")) {
-                    List<GuildChannel> channelList = event.getGuild().getChannels();
-                    IPermissionHolder = event.getGuild().getRolesByName("Muted", false).get(0);
-                    ArrayList<Permission> allowed = new ArrayList<>();
-                    ArrayList<Permission> denied = new ArrayList<>();
-                    allowed.add(Permission.MESSAGE_READ);
-                    denied.add(Permission.MESSAGE_WRITE);
-                    for (int i = 0; i < channelList.size(); i++) {
-                        channelList.get(i).getManager().putPermissionOverride(IPermissionHolder, allowed, denied).queue();
+        if (ac.AccessControl(event.getAuthor().getId())) {
+            if (index.equalsIgnoreCase(Constants.BotPrefix + "mute")) {
+                if (event.getGuild().getRolesByName("Muted", true).size() == 0) {
+                    event.getGuild().createRole().setPermissions(Permission.UNKNOWN).setName("Muted").setColor(new Color(255, 255, 255)).queue();
+                } else {
+                    try {
+                        List<GuildChannel> channelList = event.getGuild().getChannels();
+                        IPermissionHolder = event.getGuild().getRolesByName("Muted", false).get(0);
+                        Role role = (Role) IPermissionHolder;
+                        ArrayList<Permission> allowed = new ArrayList<>();
+                        ArrayList<Permission> denied = new ArrayList<>();
+                        allowed.add(Permission.MESSAGE_READ);
+                        denied.add(Permission.MESSAGE_WRITE);
+                        for (int i = 0; i < channelList.size(); i++) {
+                            channelList.get(i).getManager().putPermissionOverride(IPermissionHolder, allowed, denied).queue();
+                        }
+                        event.getGuild().addRoleToMember(other, role).queue();
+                        channel.sendMessage("Muted " + event.getGuild().getMemberById(other).getUser().getAsTag()).queue();
+                    }catch (NullPointerException | NumberFormatException e){
+                        event.getChannel().sendMessage("Err1526:The specified ID is not a valid snowflake.").queue();
                     }
                 }
+            }else if(index.equalsIgnoreCase(Constants.BotPrefix+"unmute")){
+                try {
+                    List<Role> userRole =event.getGuild().getMemberById(other).getRoles();
+                    for (int i = 0; i < userRole.size(); i++) {
+                        if (userRole.get(i).getName().equals("Muted")) {
+                            event.getGuild().removeRoleFromMember(other, userRole.get(i)).queue();
+                            event.getChannel().sendMessage("Successfully Unmuted "+event.getGuild().getMemberById(other).getUser().getAsTag()).queue();
+                            return;
+                        }
+                    }
+                    event.getChannel().sendMessage("Err435:User Does not have \"Muted\" role!").queue();
+                }catch (NullPointerException | NumberFormatException e){
+                    event.getChannel().sendMessage("Err1526:The specified ID is not a valid snowflake.").queue();
+                }
             }
-        } catch (IllegalStateException e) {
-            e.getMessage();
+        }else{
+            event.getChannel().sendMessage("This command is in testing phase \nplease contact softblue for more info...").queue();
         }
-
     }
 
     @Override
     public void onRoleCreate(@NotNull RoleCreateEvent event) {
-        roleId = event.getRole().getId();
+        List<GuildChannel> channelList = event.getGuild().getChannels();
+        IPermissionHolder = event.getGuild().getRolesByName("Muted", false).get(0);
+        Role role = (Role) IPermissionHolder;
+        ArrayList<Permission> allowed = new ArrayList<>();
+        ArrayList<Permission> denied = new ArrayList<>();
+        allowed.add(Permission.MESSAGE_READ);
+        denied.add(Permission.MESSAGE_WRITE);
+        for (int i = 0; i < channelList.size(); i++) {
+            channelList.get(i).getManager().putPermissionOverride(IPermissionHolder, allowed, denied).queue();
+        }
+        event.getGuild().addRoleToMember(other, role).queue();
+        channel.sendMessage("Created Role Muted and ").queue();
+        channel.sendMessage("Muted " + event.getGuild().getMemberById(other).getUser().getAsTag()).queue();
     }
 
-    public boolean roleExists(List<Role> roles, String roleId) {
-        for (int i = 0; i < roles.size(); i++) {
-            if (roles.get(i).getId().equalsIgnoreCase(roleId)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
     /*FileIO fileIO = new FileIO();
